901f46f0acb3fee3e367d45aa04cf2d016d52640
artem <artem@MacBook-Pro-artem.local>
2026-01-08 22:44:38 +0300

    Add all

--- 3-channel/buffered_channel.h
+++ 3-channel/buffered_channel.h
@@ -7,62 +7,49 @@
 #include <stdexcept>
 
 template <class T>
-class BufferedChannel
-{
+class BufferedChannel {
 public:
     explicit BufferedChannel(int size) : size_(size), closed_(false) {}
 
-    void Send(T value)
-    {
+    void Send(T value) {
         std::unique_lock<std::mutex> lock(mutex_);
-
-        cv_can_send_.wait(lock, [this]
+        can_send_.wait(lock, [this]
                           { return queue_.size() < this->size_ || closed_; });
-
-        if (closed_)
-        {
-            throw std::runtime_error("channel closed");
+        if (closed_) {
+            throw std::runtime_error("Channel is closed :(");
         }
-
         queue_.push(std::move(value));
-
-        cv_can_receive_.notify_one();
+        can_receive_.notify_one();
     }
 
-    std::pair<T, bool> Recv()
-    {
+    std::pair<T, bool> Recv() {
         std::unique_lock<std::mutex> lock(mutex_);
-
-        cv_can_receive_.wait(lock, [this]
+        can_receive_.wait(lock, [this]
                              { return !(queue_.empty()) || closed_; });
-
-        if (!queue_.empty())
-        {
+        if (!queue_.empty()) {
             T value = std::move(queue_.front());
             queue_.pop();
-            cv_can_send_.notify_one();
+            can_send_.notify_one();
             return {std::move(value), true};
         }
-        else
-        {
+        else {
             return {T(), false};
         }
     }
 
-    void Close()
-    {
+    void Close() {
         std::unique_lock<std::mutex> lock(mutex_);
         this->closed_ = true;
-        cv_can_receive_.notify_all();
-        cv_can_send_.notify_all();
+        can_receive_.notify_all();
+        can_send_.notify_all();
     }
 
 private:
     std::queue<T> queue_;
     std::mutex mutex_;
+    std::condition_variable can_send_;
+    std::condition_variable can_receive_;
     int size_;
-    std::condition_variable cv_can_send_;
-    std::condition_variable cv_can_receive_;
     bool closed_;
 };
 
